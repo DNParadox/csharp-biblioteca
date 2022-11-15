1b12bc5b32b14bfa19412845be8dbbf7f49b9fa3
DNParadox <m96fr@DESKTOP-NBI01C4>
2022-11-15 16:36:06 +0100

    Push con Sql in errore

--- Program.cs
+++ Program.cs
@@ -1,27 +1,43 @@
-﻿
-//Richiesta:
-//Si vuole progettare un sistema per la gestione di una biblioteca. Gli utenti si possono registrare al sistema, fornendo:
-//cognome,
-//nome,
-//email,
-//password,
-//recapito telefonico,
-//Gli utenti registrati possono effettuare dei prestiti sui documenti che sono di vario tipo (libri, DVD). I documenti sono caratterizzati da:
-//un codice identificativo di tipo stringa (ISBN per i libri, numero seriale per i DVD),
-//titolo,
-//anno,
-//settore(storia, matematica, economia, …),
-//stato(In Prestito, Disponibile),
-//uno scaffale in cui è posizionato,
-//un autore (Nome, Cognome).
-//Per i libri si ha in aggiunta il numero di pagine, mentre per i dvd la durata.
-//L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, effettuare dei prestiti registrando il periodo 
-//(Dal/Al) del prestito e il documento.
-//Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
+﻿using System.Data.SqlClient;
+
 Biblioteca Biblioteca = new Biblioteca();
 Biblioteca.GeneraDocumenti();
 Biblioteca.GeneraUtenti();
 
+string stringaDiConnessione = "Data Source=localhost;Initial Catalog=db-biblioteca;Integrated Security=True";
+
+
+using (SqlConnection connessioneSql = new SqlConnection(stringaDiConnessione));
+{
+        try
+        {
+            connessioneSql.Open();
+            string insertQuery = "INSERT INTO documenti (codice,titolo, anno, settore, scaffale, durata, numero_pagine, autore_nome, autore_cognome, stato"
+            + "VALUES (@codice, @titolo, @anno, @settore, @scaffale, @durata, @numero_pagine, @autore_nome, @autore_cognome, @stato)";
+            
+            SqlCommand insertCommand = new SqlCommand(insertQuery, connessioneSql);
+            
+            insertCommand.Parameters.Add(new SqlCommand("@codice", "codex1234"));
+            insertCommand.Parameters.Add(new SqlCommand("@titolo", "Harry Potter La Pietra Filosofale"));
+            insertCommand.Parameters.Add(new SqlCommand("@anno", 1998));
+            insertCommand.Parameters.Add(new SqlCommand("@settore", "fantasy"));
+            insertCommand.Parameters.Add(new SqlCommand("@scaffale", "1C"));
+            insertCommand.Parameters.Add(new SqlCommand("@numero_pagine",304));
+            insertCommand.Parameters.Add(new SqlCommand("@autore_nome","J.K."));
+            insertCommand.Parameters.Add(new SqlCommand("@autore_cognome","Rowlings"));
+            insertCommand.Parameters.Add(new SqlCommand("@stato", 0));
+
+            int affectedRows = insertCommand.ExecuteNonQuery();
+        }
+        catch (Exception e)
+        {
+           Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            ConnessioneSql.Close();
+        }
+}
 
 bool loop = true;
 while (loop)
